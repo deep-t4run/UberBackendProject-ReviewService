cb03b757a04949db7a4176ba9dc051796bbba958
deep-t4run <deep.t4run@gmail.com>
2025-06-18 22:07:47 +0530

    Added service layer for Review Service

--- src/main/java/com/example/uberreviewservice/repositories/DriverRepository.java
+++ src/main/java/com/example/uberreviewservice/repositories/DriverRepository.java
@@ -1,22 +1,17 @@
 package com.example.uberreviewservice.repositories;
 
-import com.example.uberreviewservice.models.Booking;
 import com.example.uberreviewservice.models.Driver;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface DriverRepository extends JpaRepository<Driver, Long> {
 
     Optional<Driver> findByIdAndLicenseNumber(long id, String licenseNumber);
 
     List<Driver> findAllByIdIn(List<Long> DriverIds);
 
-//    @Query(nativeQuery = true, value = "select * from Driver where id = :id and license_number = :license") // raw sql query, error is thrown at run time only
-//    Optional<Driver> rawFindByIdAndLicenseNumber(long id, String license);
-//
-//    @Query("From Driver as d where d.id = :id and d.licenseNumber = :license")  // hibernate query, error is thrown at compile time
-//    Optional<Driver> hqlFindByIdAndLicenseNumber(long id, String license);
 }
---
+++ src/main/java/com/example/uberreviewservice/services/ReviewService.java
@@ -0,0 +1,17 @@
+package com.example.uberreviewservice.services;
+
+import com.example.uberreviewservice.models.Review;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public interface ReviewService {
+    public Optional<Review> findReviewById(Long id);
+
+    public List<Review> findAllReviews();
+
+    public boolean deleteReviewById(Long id);
+
+}
